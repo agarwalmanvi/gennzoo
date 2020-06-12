236fc8292622ed4b25ae9d8b731f71939d9cd44b
agarwalmanvi <94.manviagarwal@gmail.com>
2020-06-12 10:29:22 +0200

    try custom neuron model with global param

--- models/neurons/poisson_const.py
+++ models/neurons/poisson_const.py
@@ -1,12 +1,28 @@
 from pygenn.genn_model import create_custom_neuron_class
+from pygenn import genn_model
+import numpy as np
 
 poisson_const = create_custom_neuron_class(
     "poisson_const",
-    param_names=["spike_train"],
+    param_names=[],
     var_name_types=[("counter", "scalar")],
     sim_code="""
-    counter += 1;
+    printf(to_string($(arr)[$(counter)]));
+    $(counter) += 1;
     """,
-    threshold_condition_code="$(spike_train)[$(counter)] == 1")
+    threshold_condition_code="")
 
 poisson_const_init = {"counter": 0}
+
+model = genn_model.GeNNModel("float", "spike_source_array")
+model.dT = 1.0
+
+ssa = model.add_neuron_population("ssa", 1, poisson_const, {}, poisson_const_init)
+arr = np.array([1, 2, 3, 4, 5])
+ssa.set_extra_global_param("arr", arr)
+
+model.build()
+model.load()
+
+while model.timestep < 3.0:
+    model.timestep()
